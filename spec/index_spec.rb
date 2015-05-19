22c7917b8f06c771d38ee05189481a11d6c23652
Cory Spitzer & Paul Ogawa <student@epicodus.com>
2015-05-19 15:21:10 -0700

    Test and define basic search functionality

--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -1,2 +1,22 @@
 require "spec_helper"
 require('./app')
+
+describe('overall page testing', {type: :feature} ) do
+  describe('visiting the homepage') do
+    it('checks that the homepage is working') do
+      visit '/'
+      expect(page).to have_content("Epicodus Reference Wiki")
+    end
+  end
+  describe('the search path') do
+    it('searches for a tag, displays results') do
+      tag = Tag.create(topic: 'activerecord')
+      tag2 = Tag.create(topic: 'sinatra')
+      visit('/')
+      fill_in('search_word', with: 'sinatra')
+      click_button('Search')
+      expect(page).to have_content(tag2.topic)
+    end
+  end
+
+end
