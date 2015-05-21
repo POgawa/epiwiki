a12aad3bc1c433ccf8bb83aff7f1ff975292f6f0
Cory Spitzer & Paul Ogawa <student@epicodus.com>
2015-05-21 10:26:59 -0700

    Test and define routes to visit the edit article page

--- db/schema.rb
+++ db/schema.rb
@@ -35,9 +35,9 @@ ActiveRecord::Schema.define(version: 20150520224153) do
   end
 
   create_table "tags", force: :cascade do |t|
+    t.string   "topic"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "topic"
   end
 
   create_table "users", force: :cascade do |t|
