c802828f6d590d7581cec75478d12e92d141092b
Guest User <student@epicodus.com>
2015-05-22 09:14:07 -0700

    Add correct info

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ helpers do
 
   def authorized?
     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == [ENV['login'], ENV['password']]
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
   end
 end
 
